2c43a9b575e0ff6ebada724bf4d51c51e50febf6
ZiM88 <renegad1929@gmail.com>
2023-07-13 10:37:56 +0300

    Use Map in viewModelFactory

--- app/src/main/java/com/example/distart/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/distart/example2/presentation/ViewModelFactory.kt
@@ -2,17 +2,15 @@ package com.example.distart.example2.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.distart.example2.domain.ExampleRepository
 import com.example.distart.example2.domain.ExampleUseCase
 import javax.inject.Inject
 
 class ViewModelFactory @Inject constructor(
-    private val exampleUseCase: ExampleUseCase
+    private val viewModels: @JvmSuppressWildcards Map<String, ViewModel>
 ): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass == ExampleViewModel::class.java) {
-            return ExampleViewModel(exampleUseCase) as T
-        }
-        throw RuntimeException("Unknown view model class $modelClass")
+        return viewModels[modelClass.simpleName] as T
     }
 }
