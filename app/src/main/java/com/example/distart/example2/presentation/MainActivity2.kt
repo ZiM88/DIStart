ca6f14e79bf9cc1e8571ba9224196bb1163e054d
ZiM88 <renegad1929@gmail.com>
2023-07-13 11:12:05 +0300

    Use Provider

--- app/src/main/java/com/example/distart/example2/presentation/MainActivity2.kt
+++ app/src/main/java/com/example/distart/example2/presentation/MainActivity2.kt
@@ -1,9 +1,7 @@
 package com.example.distart.example2.presentation
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.distart.R
@@ -11,7 +9,7 @@ import com.example.distart.example2.ExampleApp
 import com.example.distart.example2.di.DaggerApplicationComponent
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -34,10 +32,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel.method()
         viewModel2.method()
-        findViewById<TextView>(R.id.tv_hello_world).setOnClickListener {
-            Intent(this, MainActivity2::class.java).apply {
-                startActivity(this)
-            }
-        }
     }
 }
