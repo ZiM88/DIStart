42c42e969c0defc3b2909d98ec665561d626907f
ZiM88 <renegad1929@gmail.com>
2023-07-14 10:35:36 +0300

    Use subcomponent

--- app/src/main/java/com/example/distart/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/distart/example2/presentation/ViewModelFactory.kt
@@ -8,12 +8,11 @@ import com.example.distart.example2.domain.ExampleUseCase
 import javax.inject.Inject
 import javax.inject.Provider
 
-@ApplicationScope
 class ViewModelFactory @Inject constructor(
-    private val viewModelProviders: @JvmSuppressWildcards Map<String, Provider<ViewModel>>
+    private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
 ): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return viewModelProviders[modelClass.simpleName]?.get() as T
+        return viewModelProviders[modelClass]?.get() as T
     }
 }
